ca36518c443eebe77dc7fb2ad6d8c476cc7e3be2
littlecold4 <minu09252@gmail.com>
2022-07-02 01:08:29 +0900

    NumberSort3

--- algorithm/src/main/java/Sort/NumberSort3.java
+++ algorithm/src/main/java/Sort/NumberSort3.java
@@ -3,8 +3,9 @@ package Sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
-public class NumberSort {
+public class NumberSort3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
@@ -12,16 +13,7 @@ public class NumberSort {
         for(int i=0; i<num; i++){
             numList[i] = Integer.parseInt(br.readLine());
         }
-        int temp;
-        for(int i=0; i<num-1; i++){
-            for(int j=0; j<num-1; j++){
-                if(numList[j]>numList[j+1]){
-                    temp = numList[j];
-                    numList[j] = numList[j+1];
-                    numList[j+1] = temp;
-                }
-            }
-        }
+        Arrays.sort(numList);
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<num; i++){
             sb.append(numList[i]);
