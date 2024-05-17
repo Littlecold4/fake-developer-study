31f37b69c032c87a24ac68f64c9e70fcaadfcdab
littlecold4 <minu09252@gmail.com>
2024-05-17 16:33:34 +0900

    TileDecoration _ Success

---
+++ algorithm/src/main/java/DynamicProgramming/TileDecoration.java
@@ -0,0 +1,20 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TileDecoration {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[81]; dp[0] = 1; dp[1] =1;
+
+        for(int i=2; i<=N; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(2*dp[N] + 2*dp[N-1]);
+    }
+}
