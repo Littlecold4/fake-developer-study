110386418e1eb12671fa22bd6d303f083e1dc150
littlecold4 <minu09252@gmail.com>
2024-05-17 16:26:46 +0900

    BABBA _ Success

--- algorithm/src/main/java/DynamicProgramming/BABBA.java
+++ algorithm/src/main/java/DynamicProgramming/BABBA.java
@@ -8,13 +8,31 @@ public class BABBA {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
-        String[] dp = new String[46];
 
-        dp[0] ="A"; dp[1] = "B"; dp[2] = "BA";
+        int[] dpA = new int[46];
+        int[] dpB = new int[46];
 
-        for(int i =3; i<=K; i++) dp[i] = dp[i-1].replace("A","C").replace("B","BA").replace("C","B");
+        dpA[0] = 1; dpB[0] =0;
 
-        System.out.println(dp[K].replace("B","").length()+ " " + dp[K].replace("A","").length());
+        for(int i=1; i<=K; i++){
+            dpA[i] = dpB[i-1];
+            dpB[i] = dpA[i-1] + dpB[i-1];
+        }
+
+        System.out.println(dpA[K] + " " + dpB[K]);
+
+
+        /**  아래의 경우 K=45일때 문자의 갯수가 너무 많기때문에 String을 모두 저장하면 매모리 초과가 발생한다. A,B 의 숫자만 저장해서 해결
+
+//        String[] dp = new String[46];
+
+
+//        dp[0] ="A"; dp[1] = "B"; dp[2] = "BA";
+//
+//        for(int i =3; i<=K; i++) dp[i] = dp[i-1].replace("A","C").replace("B","BA").replace("C","B");
+//
+//        System.out.println(dp[K].replace("B","").length()+ " " + dp[K].replace("A","").length());
+         **/
 
     }
 }
