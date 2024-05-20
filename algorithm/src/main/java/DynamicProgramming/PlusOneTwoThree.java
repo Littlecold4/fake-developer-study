78fb79e6ee0e78ee2a2adccdc7e58f5b3ab1a345
littlecold4 <minu09252@gmail.com>
2024-05-20 13:37:35 +0900

    PlusOneTwoThree _ Success

--- algorithm/src/main/java/DynamicProgramming/PlusOneTwoThree.java
+++ algorithm/src/main/java/DynamicProgramming/PlusOneTwoThree.java
@@ -12,13 +12,12 @@ public class PlusOneTwoThree {
         int[] dp = new int[13];
         dp[0] = 0; dp[1] = 1; dp[2] =2; dp[3] =4;
         for(int i=4 ; i<11; i++){
-            dp[i] = dp[i-1] + dp[i-2]*2 + dp[i-3]*3;
-            System.out.println(dp[i]);
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
 
         for(int i=0; i<T; i++){
-            int N = Integer.parseInt(br.readLine());
-            System.out.println(dp[N]);
+            int k = Integer.parseInt(br.readLine());
+            System.out.println(dp[k]);
         }
         //결과 오류
     }
