ab0ab2691d00242261313ad841acbeef13ffef38
littlecold4 <minu09252@gmail.com>
2025-11-27 09:46:36 +0900

    SumOfNum

---
+++ algorithm/src/main/java/Greedy/SumOfNum.java
@@ -0,0 +1,19 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SumOfNum {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        long num = Long.parseLong(br.readLine());
+        long i=1;
+        while(num>=0){
+            num -=i;
+            i++;
+        }
+        System.out.println(i-2);
+
+    }
+}
