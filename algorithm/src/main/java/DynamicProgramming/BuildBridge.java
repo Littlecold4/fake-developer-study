b91131e4fc1c94571f6d34a2150c7b02108d9a1e
littlecold4 <minu09252@gmail.com>
2025-12-08 10:44:53 +0900

    BuildBridge

---
+++ algorithm/src/main/java/DynamicProgramming/BuildBridge.java
@@ -0,0 +1,31 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BuildBridge {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int T= Integer.parseInt(br.readLine());
+        int[][] dp = new int[30][30];
+
+        for(int i=0; i<30; i++){
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+        for(int i=2; i<30; i++){
+            for (int j=1 ; j<30; j++){
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }
+        }
+        for(int t = 0; t<T; t++){
+            String[] NnMStr = br.readLine().split(" ");
+            int N = Integer.parseInt(NnMStr[0]);
+            int M = Integer.parseInt(NnMStr[1]);
+
+            System.out.println(dp[M][N]);
+        }
+    }
+
+}
