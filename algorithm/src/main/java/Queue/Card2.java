546c7d01bf66f57af6977fbccb90b8d6872a1d5c
littlecold4 <minu09252@gmail.com>
2026-02-11 11:09:40 +0900

    card2

--- algorithm/src/main/java/Queue/Card2.java
+++ algorithm/src/main/java/Queue/Card2.java
@@ -3,24 +3,24 @@ package Queue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Card2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=1; i<=num; i++){
-            queue.add(i);
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i<=N; i++){
+            q.add(i);
         }
-        while(true){
-            queue.remove();
-            if(queue.size()==1){
-                System.out.println(queue.poll());
-                break;
-            }
-            queue.add(queue.poll());
+        while(q.size()!=1){
+            q.poll();
+            int num = q.poll();
+            q.add(num);
         }
+
+        System.out.println(q.poll());
     }
 }
